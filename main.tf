e24d18bd4426743e32e537e941091b9e0da8601a
hareesh98 <95620791+hareesh98@users.noreply.github.com>
2022-10-28 17:38:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,62 +1,12 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_vpc" "mainvpc" {
-  cidr_block = "10.0.0.0/16"
-  instance_tenancy = "default"
-  tags = {
-    Name = "main"
-  }
-}
-
-#Create security group with firewall rules
-resource "aws_security_group" "jenkins-sg-2022" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
-}
-
-resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
-  key_name = var.key_name
+resource "aws_instance" "test_server" {
+  ami           = var.ami 
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
-  tags= {
-    Name = var.tag_name
-  }
-}
-
-# Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
-  vpc      = true
-  instance = aws_instance.myFirstInstance.id
-tags= {
-    Name = "my_elastic_ip"
+  associate_public_ip_address = var.associate_public_ip_address
+  monitoring = var.monitoring
+  vpc_security_group_ids = var.vpc_security_group_ids
+  subnet_id = var.subnet_id
+  tags = var.tags
+  timeouts {
+    create = "60m"
   }
 }
