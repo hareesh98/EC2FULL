ad9997b5fa408e02056f4c3beef83530ed6d72fc
hareesh98 <95620791+hareesh98@users.noreply.github.com>
2022-10-28 17:39:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,47 +1,51 @@
-variable "aws_region" {
-       description = "The AWS region to create things in." 
-       default     = "ap-south-1" 
+variable "access_key" {
+  description = "aws access key"
+  type        = string
 }
 
-
-variable "instance_type" { 
-    description = "instance type for ec2" 
-    default     =  "t2.micro" 
+variable "secret_access_key" {
+  description = "aws secret access key"
+  type        = string
+}
+variable "region" {
+  description = "aws region"
+  type        = string
+  default     = "ap-south-1"
 }
 
-variable "security_group" { 
-    description = "Name of security group" 
-    default     = "Security groups to attach to ec2 instance" 
+variable "instance_type" {
+  description = "instance type"
+  type        = string
+  default     = "t2.micro"
 }
 
-variable "tag_name" { 
-    description = "Tag Name of for Ec2 instance" 
-    default     = "my-ec2-instance" 
-} 
-variable "ami_id" { 
-    description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-062df10d14676e201" 
+variable "ami" {
+  description = "instance ami"
+  type = string
 }
-variable "versioning" {
-    type        = bool
-    description = "(Optional) A state of versioning."
-    default     = true
+
+variable "associate_public_ip_address" {
+  description = "assigning public ip address"
+  type = bool
 }
-variable "acl" {
-    type        = string
-    description = " Defaults to private "
-    default     = "private"
+
+variable "monitoring" {
+  description =  "enabling monitoring"
+  type = bool
+  default = "false"
 }
-variable "bucket_prefix" {
-    type        = string
-    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-s3bucket-"
+
+variable "vpc_security_group_ids" {
+  description =  "VPC Security groups to attach to ec2 instance"
+  type = list
 }
+
+variable "subnet_id" {
+  description = "subnet id"
+  type = string
+}
+
 variable "tags" {
-    type        = map
-    description = "(Optional) A mapping of tags to assign to the bucket."
-    default     = {
-        environment = "DEV"
-        terraform   = "true"
-    }
+  description =  "ec2 instance tags"
+  type = map
 }
