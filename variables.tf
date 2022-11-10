dd034a592d75da9a8a872548959257dfded11e6b
hareesh98 <95620791+hareesh98@users.noreply.github.com>
2022-11-10 15:55:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -54,3 +54,13 @@ variable "tags" {
   description =  "ec2 instance tags"
   type = map
 }
+variable "acl" {
+    type        = string
+    description = " Defaults to private "
+    default     = "private"
+}
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
+    default     = "testingbucket-testingbucket"
+}
